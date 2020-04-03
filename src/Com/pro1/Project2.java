e2443ce061b53079fc66d2dbf6d69a8517885d19
pandurang <pandurang@pandurang-Lenovo-G500s>
2020-04-03 22:47:21 +0530

    commiting project2 to BranchX

---
+++ src/Com/pro1/Project2.java
@@ -0,0 +1,14 @@
+package Com.pro1;
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int sum = (a+b);
+		System.out.println(sum);
+
+	}
+
+}

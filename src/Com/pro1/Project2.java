dc073107b76abb13f316f357d6a23bd96298d4d6
pandurang <pandurang@pandurang-Lenovo-G500s>
2020-04-03 23:39:07 +0530

    added additional check

--- src/Com/pro1/Project2.java
+++ src/Com/pro1/Project2.java
@@ -6,7 +6,8 @@ public class Project2 {
 		// TODO Auto-generated method stub
 		int a = 10;
 		int b = 20;
-		int sum = (a+b);
+		int c = 100;
+		int sum = (a+b+c);
 		System.out.println(sum);
 
 	}
